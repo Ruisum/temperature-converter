8c39fdf13a611cee35ea0089b32bc6592926f6d9
Lewis <lewis@fake.co.uk>
2022-01-15 20:42:23 +0000

    Updated command names and aliases

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ const { celsiusToFahrenheit, fahrenheitToCelsius } = require('../src/converter')
 const { program } = require('commander');
 program.version('0.0.1');
 
-program.command('ftoc')
+program.command('fahrenheitToCelsius')
+    .alias('ftoc')
     .description('Converts a fahrenheit temperature into celsius')
     .argument('[temperature]', 'Temperature in fahrenheit to be converted to celsius')
     .action(temperature => {
         console.log(temperature + ' fahrenheit is ' + fahrenheitToCelsius(temperature) + ' celsius');
     });
 
-program.command('ctof')
+program.command('celsiusToFahrenheit')
+    .alias('ctof')
     .description('Converts a celsius temperature into fahrenheit')
     .argument('[temperature]', 'Temperature in celsius to be converted to fahrenheit')
     .action(temperature => {
-        console.log(temperature + ' celsius is ' + fahrenheitToCelsius(temperature) + ' fahrenheit');
+        console.log(temperature + ' celsius is ' + celsiusToFahrenheit(temperature) + ' fahrenheit');
     });
 
 program.parse(process.argv);
