76e59c2abdeeda0f363fff134f5467b57ee447db
Lewis <lewis@fake.co.uk>
2022-01-15 18:14:49 +0000

    Added fahrenheit to celsius function and tests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const celsiusToFahrenheit = CTemperature => {
     const FTemperature = CTemperature * 1.8 + 32;
-    return FTemperature;
+    return Math.round(FTemperature);
 };
 
-module.exports = {celsiusToFahrenheit};
+const fahrenheitToCelsius = FTemperature => {
+    const CTemperature = (FTemperature - 32) / 1.8;
+    return Math.round(CTemperature);
+};
+
+module.exports = {celsiusToFahrenheit, fahrenheitToCelsius};
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,14 +1,29 @@
-const { celsiusToFahrenheit } = require('../src/index');
+const { celsiusToFahrenheit, fahrenheitToCelsius } = require('../src/index');
 
-test("30 celsius is 86 fahrenheit", () => {
-    expect(celsiusToFahrenheit(30)).toBe(86);
+describe("Celsius to Fahrenheit", () => {
+    test("30 celsius is 86 fahrenheit", () => {
+        expect(celsiusToFahrenheit(30)).toBe(86);
+    });
+    
+    test("0 celsius is 32 fahrenheit", () => {
+        expect(celsiusToFahrenheit(0)).toBe(32);
+    });
+    
+    test("100 celsius is 212", () => {
+        expect(celsiusToFahrenheit(100)).toBe(212);
+    });
 });
 
-test("0 celsius is 32 fahrenheit", () => {
-    expect(celsiusToFahrenheit(0)).toBe(32);
-});
+describe("Fahrenheit to Celsius", () => {
+    test("100 fahreinheit is 38 celsius", () => {
+        expect(fahrenheitToCelsius(100)).toBe(38);
+    });
 
-test("100 celsius is 212", () => {
-    expect(celsiusToFahrenheit(100)).toBe(212);
-});
+    test("0 fahrenheit is -18 celsius", () => {
+        expect(fahrenheitToCelsius(0)).toBe(-18);
+    });
 
+    test("200 fahrenheit is 93 celsius", () => {
+        expect(fahrenheitToCelsius(200)).toBe(93);
+    });
+});
